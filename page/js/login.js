6e02effa298f2ed4da5d6665e7736be41331d3e0
liuxiaoyu <liuxiaoyu@kingxunlian.com>
2019-02-12 16:03:41 +0800

    登录修改

--- page/js/login.js
+++ page/js/login.js
@@ -2,18 +2,19 @@ $(document).ready(function(){
     $("#submits").click(function (){
         var uname = $("#username").val();
         var pas = $("#password").val();
-        var jsondata = {"username":uname,"password":pas};
+        var jsondata = {"uname":uname,"password":pas};
 
         $.ajax({
             url:'http://localhost:8090/login',
-            data:jsondata,
+            data:JSON.stringify(jsondata),
             type:"POST",
+            contentType: "application/json;charset=UTF-8",
             cache:false,//false是不缓存，true为缓存
             async:true,//true为异步，false为同步
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Access-Control-Allow-Credentials',true);
-            xhr.setRequestHeader('Access-Control-Allow-Origin','*');
-            },
+            // beforeSend: function (xhr) {
+            //     xhr.setRequestHeader('Access-Control-Allow-Credentials',true);
+            // xhr.setRequestHeader('Access-Control-Allow-Origin','*');
+            // },
             success:function(data){
                 alert(data['jwtToken']);
                 sessionStorage.setItem("token",data['jwtToken']);
