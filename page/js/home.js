c147f468d4680cd47fb2c99384fa820cf3143904
liuxiaoyu <liuxiaoyu@kingxunlian.com>
2019-02-15 17:40:44 +0800

    增加用户退出接口

--- page/js/home.js
+++ page/js/home.js
@@ -26,11 +26,29 @@ $(document).ready(function(){
 
     if(sessionStorage.getItem("token") == null || sessionStorage.getItem("token") == "") {
         alert("Token已过期，请重新登录！");
+        // toastr.warning("Token已过期，请重新登录！");
         location.href = "login.html";
     }
 
     $("#logout").click(function (){
-        sessionStorage.removeItem("token");
-        location.href = "login.html";
+        $.ajax({
+            url:'http://localhost:8090/loginOut',
+            data:JSON.stringify(jdata),
+            type:"POST",
+            dataType:"json",
+            cache:false,//false是不缓存，true为缓存
+            async:true,//true为异步，false为同步
+            contentType: "application/json;charset=UTF-8",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization','Bearer' + sessionStorage.getItem('token'));
+            },
+            success:function(data){
+                sessionStorage.removeItem("token");
+                location.href = "login.html";
+            },
+            error(exc) {
+                console.info(exc);
+            }
+        });
     });
 });
